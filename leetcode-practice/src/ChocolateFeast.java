bf717f3c7891784ee7b53ebf149f087fc4c9e1d7
Samy Gerges <samygerges2010@gmail.com>
2020-04-10 12:55:47 +0200

    leetcode problems

---
+++ leetcode-practice/src/ChocolateFeast.java
@@ -0,0 +1,27 @@
+
+public class ChocolateFeast
+{
+    public static void main(String[] args)
+    {
+        System.out.println(chocolateFeast(6, 2, 2));
+    }
+
+
+    static int chocolateFeast(int n, int c, int m)
+    {
+        int eaten = 0;
+        int wrappers = 0;
+        for (int i = n - c; i >= 0; i -= c)
+        {
+            eaten++;
+            wrappers++;
+            if (wrappers == m)
+            {
+                i += c;
+                wrappers = 0;
+            }
+        }
+        return eaten;
+    }
+
+}
---
+++ leetcode-practice/src/MinStack.java
@@ -0,0 +1,56 @@
+import java.util.Stack;
+
+import javafx.util.Pair;
+
+public class MinStack
+{
+    private Stack<Pair<Integer, Integer>> stack;
+
+
+    /** initialize your data structure here. */
+    public MinStack()
+    {
+        stack = new Stack<>();
+    }
+
+
+    public void push(int x)
+    {
+        if (stack.isEmpty())
+        {
+            stack.push(new Pair<Integer, Integer>(x, x));
+        }
+        else
+        {
+            int min = stack.peek().getValue();
+            stack.push(new Pair<Integer, Integer>(x, Math.min(x, min)));
+        }
+    }
+
+
+    public void pop()
+    {
+        stack.pop();
+    }
+
+
+    public int top()
+    {
+        return stack.peek().getKey();
+    }
+
+
+    public int getMin()
+    {
+        return stack.peek().getValue();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
