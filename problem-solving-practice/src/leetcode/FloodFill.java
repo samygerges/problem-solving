453228d1e8e05f757cdf55a5af43eb015062e793
Samy Gerges <samygerges2010@gmail.com>
2020-05-11 16:29:39 +0200

    Leetcode Flood Fill problem

--- problem-solving-practice/src/leetcode/FloodFill.java
+++ problem-solving-practice/src/leetcode/FloodFill.java
@@ -2,22 +2,21 @@ package leetcode;
 
 public class FloodFill {
 
-	private static int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-
-	public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-		boolean[][] visited = new boolean[image.length][image[0].length];
-		fillPoints(image, sr, sc, newColor, image[sr][sc], visited);
-		return image;
-	}
-
-	private void fillPoints(int[][] image, int sr, int sc, int newColor, int color, boolean[][] visited) {
-		image[sr][sc] = newColor;
-		visited[sr][sc] = true;
-		for (int[] dir : directions) {
-			if (sr + dir[0] < image.length && sr + dir[0] > -1 && sc + dir[1] < image[sr].length && sc + dir[1] > -1
-					&& !visited[sr + dir[0]][sc + dir[1]] && image[sr + dir[0]][sc + dir[1]] == color) {
-				fillPoints(image, sr + dir[0], sc + dir[1], newColor, color, visited);
-			}
-		}
-	}
+	private static int[][] directions = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(newColor == image[sr][sc]) return image;
+        fillPoints(image, sr, sc, newColor, image[sr][sc]);
+        return image;
+    }
+    
+    private void fillPoints(int[][] image, int sr, int sc, int newColor, int color)
+    {   
+        image[sr][sc] = newColor;
+        for(int[] dir : directions) {
+            if(sr + dir[0] < image.length && sr + dir[0] >-1 && sc + dir[1] < image[sr].length && 
+               sc + dir[1] > -1 && image[sr + dir[0]][sc + dir[1]] == color) {
+                fillPoints(image, sr + dir[0], sc + dir[1], newColor, color);    
+            }
+        }    
+    }
 }
