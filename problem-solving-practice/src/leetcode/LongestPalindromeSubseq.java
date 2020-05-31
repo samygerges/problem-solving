d2ccdb6db2b74930290f5b1950841fc7b3bee651
Samy Gerges <samygerges2010@gmail.com>
2020-05-31 12:46:30 +0200

    leetcode problem

--- problem-solving-practice/src/leetcode/LongestPalindromeSubseq.java
+++ problem-solving-practice/src/leetcode/LongestPalindromeSubseq.java
@@ -7,23 +7,19 @@ public class LongestPalindromeSubseq {
 	}
 
 	public int longestPalindromeSubseq(String s) {
-		if (s == null || s.length() == 0) {
-			return 0;
-		}
+		int[][] dp = new int[s.length()][s.length()];
 
-		int[] dp = new int[s.length()];
-		dp[0] = 1;
-
-		int max = 1;
-		for (int i = 1; i < s.length(); i++) {
-			dp[i] = 1;
-			for (int j = 0; j < i; j++) {
+		for (int i = 0; i < s.length(); i++) {
+			dp[i][i] = 1;
+			for (int j = i - 1; j >= 0; j--) {
 				if (s.charAt(i) == s.charAt(j)) {
-					dp[i] = Math.max(dp[i], dp[j] + 1);
+					dp[i][j] = dp[i - 1][j + 1] + 2;
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j + 1]);
 				}
 			}
-			max = Math.max(max, dp[i]);
 		}
-		return max;
+
+		return dp[s.length() - 1][0];
 	}
 }
