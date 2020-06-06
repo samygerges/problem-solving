915f97e6db834e4d576eaa42b99a93ec054470aa
Samy Gerges <samygerges2010@gmail.com>
2020-06-06 21:40:56 +0200

    leetcode problem

--- problem-solving-practice/src/leetcode/DiceRollsWithTargetSum.java
+++ problem-solving-practice/src/leetcode/DiceRollsWithTargetSum.java
@@ -28,4 +28,19 @@ public class DiceRollsWithTargetSum {
 		}
 		return dp.get(target);
 	}
+
+	public int numRollsToTarget2(int d, int f, int target) {
+		int[][] dp = new int[d + 1][target + 1];
+		dp[0][0] = 1;
+		for (int i = 1; i <= d; i++) {
+			for (int j = 1; j <= target; j++) {
+				for (int k = 1; k <= f; k++) {
+					if (j - k >= 0) {
+						dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % (int) mod;
+					}
+				}
+			}
+		}
+		return dp[d][target];
+	}
 }
