364153f733e5aae2d182e913a7a6bd012efb1204
Samy Gerges <samygerges2010@gmail.com>
2020-06-16 21:43:32 +0200

    HackerRank problem

--- problem-solving-practice/src/hackerrank/CounterGame.java
+++ problem-solving-practice/src/hackerrank/CounterGame.java
@@ -1,5 +1,29 @@
 package hackerrank;
 
 public class CounterGame {
+	static String counterGame(long n) {
+		int counter = 0;
+		while (n > 1) {
+			long x = sqrt(n);
+			if (x == 0) {
+				n /= 2;
+			} else {
+				n = n - x;
+			}
+			counter++;
+		}
+		return counter % 2 == 0 ? "Richard" : "Louise";
+	}
 
+	static long sqrt(long n) {
+		for (int i = 0; i < 64; i++) {
+			if (n == Math.pow(2, i)) {
+				return 0;
+			}
+			if (n < Math.pow(2, i)) {
+				return (long) Math.pow(2, i - 1);
+			}
+		}
+		return 0;
+	}
 }
