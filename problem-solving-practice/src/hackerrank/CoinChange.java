d96b009af429e8e8ec273683c01f2e75b17c276e
Samy Gerges <samygerges2010@gmail.com>
2020-10-11 19:19:32 +0200

    hackerrank

--- problem-solving-practice/src/hackerrank/CoinChange.java
+++ problem-solving-practice/src/hackerrank/CoinChange.java
@@ -1,26 +1,40 @@
 package hackerrank;
 
-public class CoinChange {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
 
-	public static int change(int amount, int[] coins) {
-		return change(amount, coins, 0, 0);
+import static java.util.stream.Collectors.toList;
+
+public class CoinChange
+{
+	public static long getWays(int n, List<Integer> c)
+	{
+		long[] dp = new long[n + 1];
+        dp[0] = 1;
+        for (int i = 0; i < c.size(); i++)
+        {
+            for (int j = c.get(i); j <= n; j++)
+            {
+                dp[j] += dp[j - c.get(i)];
+            }
+        }
+
+        return dp[n];
 	}
 
-	private static int change(int amount, int[] coins, int current, int start) {
-		if (current == amount)
-			return 1;
-		else if (current > amount)
-			return 0;
-
-		int count = 0;
-		for (int i = start; i < coins.length; i++) {
-			count += change(amount, coins, current + coins[i], start + i);
-		}
-
-		return count;
-	}
-
-	public static void main(String[] args) {
-		System.out.println(change(500, new int[] { 1, 2, 5 }));
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+		int n = Integer.parseInt(firstMultipleInput[0]);
+		int m = Integer.parseInt(firstMultipleInput[1]);
+		List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt).collect(toList());
+		// Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+		long ways = getWays(n, c);
+		System.out.println(ways);
+		bufferedReader.close();
 	}
 }
