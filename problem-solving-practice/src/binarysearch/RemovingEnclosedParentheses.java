2c1f8118f89b9da2d7c3d0abd8650bf29dd0383e
Samy Gerges <samygerges2010@gmail.com>
2020-11-08 16:39:50 +0100

    binarysearch problem

--- problem-solving-practice/src/binarysearch/RemovingEnclosedParentheses.java
+++ problem-solving-practice/src/binarysearch/RemovingEnclosedParentheses.java
@@ -1,6 +1,8 @@
 package binarysearch;
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RemovingEnclosedParentheses {
 	public static void main(String[] args) {
@@ -8,38 +10,24 @@ public class RemovingEnclosedParentheses {
 	}
 
 	public String solve(String s) {
-		int count = 0;
-
-		ArrayDeque<Character> stack = new ArrayDeque<>();
+		Map<Integer, Integer> map = new HashMap<>();
+		ArrayDeque<Integer> stack = new ArrayDeque<>();
 		for (int i = 0; i < s.length(); i++) {
-			if (stack.size() == s.length() - i) {
-				boolean check = true;
-				for (int j = i; j < s.length(); j++) {
-					if (s.charAt(j) != ')') {
-						check = false;
-						break;
-					}
-				}
-				if (check) {
-					count = stack.size();
-					break;
-				}
-			}
 			if (s.charAt(i) == '(') {
-				stack.push('(');
+				stack.push(i);
 			} else if (s.charAt(i) == ')') {
-				stack.removeFirst();
+				int index = stack.pop();
+				map.put(index, i);
 			}
-			if (stack.isEmpty()) {
+		}
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(' && map.get(i) == s.length() - i - 1) {
+				count++;
+			} else {
 				break;
 			}
 		}
-
-		String ans = s;
-		if (count > 0) {
-			ans = s.substring(count);
-			ans = ans.substring(0, ans.length() - count);
-		}
-		return ans;
+		return s.substring(count, s.length() - count);
 	}
 }
