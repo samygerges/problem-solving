884e67225c48ff2c29e5896755cbcae393e9f68d
Samy Gerges <samy.gerges@visual-meta.com>
2021-03-28 12:23:18 +0200

    rename class

--- problem-solving-practice/src/googlecodejam2021Qualification/ReversortEngineering.java
+++ problem-solving-practice/src/googlecodejam2021Qualification/ReversortEngineering.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
-public class Solution
+public class ReversortEngineering
 {
 	public static void main(String[] args)
 	{
