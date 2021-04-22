4f4516ae6ef488d7637b1d248b6781803ed0b677
Samy Gerges <samygerges2010@gmail.com>
2021-04-22 10:40:28 +0200

    fix loop

--- problem-solving-practice/src/leetcode/BrickWall.java
+++ problem-solving-practice/src/leetcode/BrickWall.java
@@ -13,7 +13,7 @@ public class BrickWall
 		for (int i = 0; i < wall.size(); i++)
 		{
 			int sum = 0;
-			for (int j = 0; j < wall.get(i).size(); j++)
+			for (int j = 0; j < wall.get(i).size() - 1; j++)
 			{
 				sum += wall.get(i).get(j);
 				map.put(sum, map.getOrDefault(sum, 0) + 1);
