0b76c96733c65a09b485cccfe180fd728771ca41
Samy Gerges <samygerges2010@gmail.com>
2021-04-22 10:39:26 +0200

    leetcode problem

---
+++ problem-solving-practice/src/leetcode/BrickWall.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BrickWall
+{
+	public int leastBricks(List<List<Integer>> wall)
+	{
+		Map<Integer, Integer> map = new HashMap<>();
+		int count = 0;
+		for (int i = 0; i < wall.size(); i++)
+		{
+			int sum = 0;
+			for (int j = 0; j < wall.get(i).size(); j++)
+			{
+				sum += wall.get(i).get(j);
+				map.put(sum, map.getOrDefault(sum, 0) + 1);
+				count = Math.max(count, map.get(sum));
+			}
+		}
+
+		return wall.size() - count;
+	}
+}
