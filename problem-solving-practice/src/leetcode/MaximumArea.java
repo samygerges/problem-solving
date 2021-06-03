b0cf1cf6b1d57039f66a7bc61775298196c97110
Samy Gerges <samygerges2010@gmail.com>
2021-06-03 17:15:21 +0200

    leetcode problem

---
+++ problem-solving-practice/src/leetcode/MaximumArea.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MaximumArea
+{
+	public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts)
+	{
+		Arrays.sort(horizontalCuts);
+		Arrays.sort(verticalCuts);
+		int mod = (int) 1e9+7;
+
+		int maxH = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length - 1]);
+		;
+		int maxW = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length - 1]);
+
+		for (int i = 1; i < horizontalCuts.length; i++)
+		{
+			maxH = Math.max(maxH, horizontalCuts[i] - horizontalCuts[i - 1]);
+		}
+
+		for (int i = 1; i < verticalCuts.length; i++)
+		{
+			maxW = Math.max(maxW, verticalCuts[i] - verticalCuts[i - 1]);
+		}
+
+		return (int)((long)maxH * maxW % mod);
+	}
+}
