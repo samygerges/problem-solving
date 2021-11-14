b0863b298135c90dcc0426586172b4e980988b1c
Samy Gerges <samygerges2010@gmail.com>
2021-11-14 22:17:17 +0100

    update solution for Partition Equal subset sum to DP solution

--- problem-solving-practice/src/leetcode/PartitionEqualSubsetSum.java
+++ problem-solving-practice/src/leetcode/PartitionEqualSubsetSum.java
@@ -1,64 +1,26 @@
 package leetcode;
 
-import java.util.List;
-
 public class PartitionEqualSubsetSum {
 
-	public boolean canPartition(int[] nums) {
-		int sum = 0;
-		for (int element : nums) {
-			sum += element;
-		}
-		if (sum % 2 != 0) {
-			return false;
-		}
-		Boolean[] visited = new Boolean[sum / 2 + 1];
-		return backtrack(nums, 0, sum / 2, visited);
-	}
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int max = 0;
+        for (int element : nums) {
+            sum += element;
+            max = Math.max(max, element);
+        }
+        if (sum % 2 != 0 || max > sum / 2) return false;
+        Boolean[] dp = new Boolean[(sum / 2) + 1];
+        return helper(nums, dp, 0, sum / 2);
+    }
 
-	private boolean backtrack(int[] nums, int pos, int sum, Boolean[] visited) {
-		if (sum == 0) {
-			return true;
-		}
-		if (visited[pos] != null) {
-			return visited[pos];
-		}
-
-		for (int i = pos; i < nums.length; i++) {
-			if (sum - nums[i] >= 0) {
-				if (backtrack(nums, i + 1, sum - nums[i], visited)) {
-					return true;
-				}
-			}
-		}
-
-		visited[sum] = false;
-		return false;
-	}
-
-	private boolean canPartition(int[] nums, List<Integer> list, int index, int sum) {
-		if (index >= nums.length)
-			return false;
-		int sumList = sum(list);
-		if (sumList * 2 == sum && list.size() < nums.length)
-			return true;
-		for (int i = index; i < nums.length; i++) {
-			list.add(nums[i]);
-			boolean check = canPartition(nums, list, i + 1, sum);
-			if (check)
-				return true;
-			list.remove(list.size() - 1);
-		}
-		return false;
-	}
-
-	private int sum(List<Integer> a) {
-		int sum = a.stream().mapToInt(i -> i.intValue()).sum();
-		return sum;
-	}
-
-	public static void main(String[] args) {
-		PartitionEqualSubsetSum a = new PartitionEqualSubsetSum();
-		System.out.println(a.canPartition(new int[] { 1, 2, 5 }));
-	}
+    private boolean helper(int[] nums, Boolean[] dp, int index, int sum) {
+        if (index == nums.length) {
+            return sum == 0;
+        }
+        if (sum < 0) return false;
+        if (sum == 0) return true;
+        if (dp[sum] != null) return dp[sum];
+        return dp[sum] = helper(nums, dp, index + 1, sum - nums[index]) || helper(nums, dp, index + 1, sum);
+    }
 }
