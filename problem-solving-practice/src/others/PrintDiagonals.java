de15bbdd21d4b19b495baac82f722d1378fbf5fd
Samy Gerges <samygerges2010@gmail.com>
2022-01-10 14:59:37 +0200

    Leetcode problem

---
+++ problem-solving-practice/src/others/PrintDiagonals.java
@@ -0,0 +1,42 @@
+package others;
+
+import java.util.ArrayDeque;
+
+public class PrintDiagonals {
+
+    public static void main(String[] args) {
+        //int[][] arr = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] arr = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}};
+        int n = arr.length;
+        int m = arr[0].length;
+        boolean[][] visited = new boolean[n][m];
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0, 0});
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                int[] e = queue.poll();
+                if (!visited[e[0]][e[1]]) {
+                    visited[e[0]][e[1]] = true;
+                    System.out.print(arr[e[0]][e[1]] + " ");
+                }
+                if (e[1] + 1 < m) {
+                    int r = e[0];
+                    int c = e[1] + 1;
+                    for (int i = c; i >= 0; i--) {
+                        if (r < n) {
+                            queue.add(new int[]{r, i});
+                            r++;
+                        }
+                    }
+                }
+                size--;
+            }
+            System.out.println();
+        }
+    }
+}
